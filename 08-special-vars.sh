03e4f32b01b55522f784ede5ac62b803ce33d802
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-08-14 08:42:24 +0530

    conversations added

---
+++ 08-special-vars.sh
@@ -0,0 +1,19 @@
+#/!bin/bash
+
+echo "vars $1 $2 $3"
+
+echo "All vars passed are $@"
+
+echo "Number of vars are $#"
+
+echo "Script name is $0"
+
+echo "Present working directory is $PWD"
+
+echo "Home directory is $HOME"
+
+echo "User running the script is $USER"
+
+echo "Process id of current script is $$"
+
+echo "Process id of last command in background is $!"
