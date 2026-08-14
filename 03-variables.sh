9ee98f1d11eecf984a6893a461485c4033630730
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-08-14 07:12:43 +0530

    conversations added

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=Rama
+PERSON2=Krishna
+
+echo "${PERSON1}:: Hi $PERSON2!"
+
+echo "${PERSON2}:: Hello $PERSON1!"
+
+echo "$PERSON1:: How are you?"
+
+echo "$PERSON2:: I am fine. How are you?"
