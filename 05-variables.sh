80ff8df270e3963d67f88f9ecf2591c0230f187b
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-08-14 07:30:12 +0530

    conversations added

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,10 +4,8 @@ echo "Please enter your username::"
 
 read USERNAME
 
-echo "username - $USERNAME"
-
 echo "Please enter your password::"
 
 read -s PASSWORD
 
-echo "password - $PASSWORD"
+echo "username - $USERNAME"
