edac1400cabf4afd66a136fa66a4a87d67f47596
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-08-14 07:00:43 +0530

    conversations added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 #this line is commented
 
---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Rama:: Hi Krishna!"
+
+echo "Krishna:: Hello Rama!"
+
+echo "Rama:: How are you?"
+
+echo "Krishna:: I am fine. How are you?"
