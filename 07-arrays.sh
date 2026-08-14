261f653649acdb5f2f1a3940cba1dd854a68ef26
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-08-14 08:36:50 +0530

    conversations added

---
+++ 07-arrays.sh
@@ -0,0 +1,11 @@
+#/!bin/bash
+
+MOVIES=("kushi" "gabbar singh" "OG")
+
+echo "First BB was ${MOVIES[0]}"
+
+echo "Second BB was ${MOVIES[1]}"
+
+echo "Third BB was ${MOVIES[2]}"
+
+echo "Pawan Kalyan movies are ${MOVIES[@]}"
