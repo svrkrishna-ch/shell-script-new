86903691b3a9bf293f3a3d0970166b309747846c
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-08-22 06:31:24 +0530

    shell script

---
+++ 10-install-scripts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute the script"
+    exit 1 #other than 0
+fi
+
+dnf install mysqll -y
