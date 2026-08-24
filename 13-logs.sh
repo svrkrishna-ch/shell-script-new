bd30253ed2ed5f854493fc9541416375992db970
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-08-24 08:27:58 +0530

    shell script

--- 13-logs.sh
+++ 13-logs.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." f1)
+LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
