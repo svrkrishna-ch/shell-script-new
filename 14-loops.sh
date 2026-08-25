01ac109743ed323994480fa4037cc85718f57ce0
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-08-26 05:14:22 +0530

    shell script

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 
-for i in {0..10}
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "Installing $2 ... $R FAILED $N"
+    else
+        echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
+}
+
+echo "Script started executed at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute the script"
+    exit 1 #other than 0
+fi
+
+for package in $@
 do
-    echo $i
+    dnf list installed $package &>>$LOG_FILE_NAME
+    if [ $1 -ne 0 ]
+    then
+        dnf install $package -y &>>$LOG_FILE_NAME
+        VALIDATE $? "Installing $package"
+    else
+        echo -e "$package is already ... $Y INSTALLED $N"
+    fi
 done
