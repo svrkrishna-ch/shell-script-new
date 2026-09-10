d770e5fd3d3e0236cbfbd7e2f25e996e169b89f4
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-09-10 05:57:44 +0530

    expense shell

---
+++ 17-backup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+USAGE(){
+    echo -e "$R USAGE:: $N sh 17-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    exit 1
+}
+
+mkdir -p /home/ec2-user/shellscript-logs
+
+if [ $# -ne 2 ]
+then
+    USAGE
+fi
+
+echo "Script started executed at: $TIMESTAMP" &>>$LOG_FILE_NAME
