c5f20b7ec512ab3b693dd6f96a5f83aead3653ba
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-09-10 06:28:10 +0530

    expense shell

--- 17-backup.sh
+++ 17-backup.sh
@@ -55,7 +55,7 @@ echo "Script started executed at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-if [ -n $FILES ]
+if [ -n "$FILES" ]
 then
     echo "Files are: $FILES"
 else
