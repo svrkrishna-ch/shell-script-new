6078d4110c29d6441ff24515fed99ec713655796
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-09-10 08:53:34 +0530

    expense shell

--- 17-backup.sh
+++ 17-backup.sh
@@ -14,7 +14,7 @@ TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 USAGE(){
-    echo -e "$R USAGE:: $N sh 17-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    echo -e "$R USAGE:: $N backup <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
     exit 1
 }
 
