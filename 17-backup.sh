a7a4a5aeebd521adeffd7dd2bea1abe1441c9b2f
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-09-10 08:14:55 +0530

    expense shell

--- 17-backup.sh
+++ 17-backup.sh
@@ -9,7 +9,7 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14}
 LOGS_FOLDER="/home/ec2-user/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1)
+LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
