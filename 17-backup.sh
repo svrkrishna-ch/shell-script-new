49554b370e8b4dff1877f657557bd45231eaf64c
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-09-10 06:27:00 +0530

    expense shell

--- 17-backup.sh
+++ 17-backup.sh
@@ -55,4 +55,9 @@ echo "Script started executed at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-echo Files are $FILES
+if [ -n $FILES ]
+then
+    echo "Files are: $FILES"
+else
+    echo "No files found older than $DAYS"
+fi
