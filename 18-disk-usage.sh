5fa714c727bb048a0a84f98aef585d5ae9f48a25
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-09-17 08:42:00 +0530

    expense shell

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -16,4 +16,4 @@ done <<< $DISK_USAGE
 
 echo -e "Message: $MSG"
 
-echo "$MSG" | mutt -s "High Disk Usage" info@joindevops.com
+echo "$MSG" | mutt -s "High Disk Usage" ramakrishna.chilla074@gmail.com
