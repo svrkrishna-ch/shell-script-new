9170b51547eecd7a256d77e707d05a44964a0496
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-09-17 08:23:52 +0530

    expense shell

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -5,7 +5,7 @@ DISK_THRESHOLD=5
 
 while read -r line
 do
-   USAGE=$(echo $line | awk -F " " '{Print $6F}')
-   PARTITION=$(echo $line | awk -F " " '{Print $NF}')
-   echo "Partition: $PARTITION , Usage: $USAGE"
+    USAGE=$(echo $line | awk -F " " '{Print $6F}')
+    PARTITION=$(echo $line | awk -F " " '{Print $NF}')
+    echo "Partition: $PARTITION , Usage: $USAGE"
 done <<< $DISK_USAGE
