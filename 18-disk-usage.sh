d941b4559d87e5a49069969ec1a8e1df58ef7230
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2026-09-17 08:35:23 +0530

    expense shell

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -7,5 +7,9 @@ while read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    echo "Partition: $PARTITION , Usage: $USAGE"
+    # echo "Partition: $PARTITION , Usage: $USAGE"
+    if [ $USAGE - ge $DISK_THRESHOLD ]
+    then
+        MSG="High disk usage on partition: $PARTITION Usage is $USAGE"
+    fi
 done <<< $DISK_USAGE
